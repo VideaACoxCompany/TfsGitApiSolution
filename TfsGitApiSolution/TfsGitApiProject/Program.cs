033632a3b659a9c060455ec9ac069c5af5c764b1
Chu, Johnny (CMG-Videa) <Johnny.Chu@Videa.tv>
2017-10-30 14:13:33 -0400

    add solution

---
+++ TfsGitApiSolution/TfsGitApiProject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetProjects();
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+
+        
+
+        public static async void GetProjects()
+        {
+            try
+            {
+                var personalaccesstoken = "4lck6b2fcjnfrddl5tpiru356xbkb2chgo4du4nirhor4fxmmqka";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                        Convert.ToBase64String(
+                            System.Text.ASCIIEncoding.ASCII.GetBytes(
+                                string.Format("{0}:{1}", "", personalaccesstoken))));
+
+                    using (HttpResponseMessage response = client.GetAsync(
+                        "https://tfs.videa.tv/tfs/Videa/_apis/projects").Result)
+                    {
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(responseBody);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
+
+}
