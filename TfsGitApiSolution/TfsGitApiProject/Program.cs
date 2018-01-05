91883a232ea7148a4b75042917a3589b899ab10c
Chu, Johnny (CMG-Videa) <Johnny.Chu@Videa.tv>
2018-01-05 17:51:23 -0500

    string format output

--- TfsGitApiSolution/TfsGitApiProject/Program.cs
+++ TfsGitApiSolution/TfsGitApiProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Mail;
 using System.Security.Cryptography.X509Certificates;
 using Newtonsoft.Json;
 using TfsGitApiProject.Entities;
@@ -14,11 +15,12 @@ namespace TfsGitApiProject
     {
         static void Main(string[] args)
         {
+
             GetResults();
             Console.Read();
         }
 
-        
+
 
         public static async void GetResults()
         {
@@ -33,27 +35,29 @@ namespace TfsGitApiProject
                 //repos.Value.ForEach(i => Console.WriteLine(i.Name + " " + i.Id));
 
 
-                var repoNameList = new List<string> {"vom", "order-management", "support", "billing", "sellers", "sss-services", "data-integration"};
+                var repoNameList = new List<string>
+                {
+                    "vom",
+                    "order-management",
+                    "support",
+                    "billing",
+                    "sellers",
+                    "sss-services",
+                    "data-integration"
+                };
 
-                //var repoId = repoService.FindId("vom");
                 var repos = repoService.FindNameAndIds(repoNameList);
 
                 var gitStatService = new GitStatsService();
-                var branchesImInterested = new[] { "dev", "release-91" };
+                var branchesImInterested = new[] {"dev", "release-93"};
 
                 foreach (var repo in repos)
                 {
                     var gitStat = await gitStatService.GetResult(repo.Key);
-                    //gitStat.Value.ForEach(i => Console.WriteLine($"Branch {i.Name} is {i.BehindCount} commit(s) behind master branch"));
-                    gitStat.Value.Where(i => branchesImInterested.Contains(i.Name)).ToList().ForEach(i => Console.WriteLine($"Repo: {repo.Value} Branch {i.Name} is {i.BehindCount} commit(s) behind master branch"));
+                    gitStat.Value.Where(i => branchesImInterested.Contains(i.Name)).ToList().ForEach(i =>
+                        Console.WriteLine($"Repo: {repo.Value.PadRight(20)} Branch {i.Name.PadRight(20)} is {i.BehindCount.PadRight(5)} commit(s) behind master branch"));
 
                 }
-
-
-                //send email
-                //SmtpService smtpService = new SmtpService();
-                //smtpService.SendEmail();
-
 
             }
             catch (Exception ex)
