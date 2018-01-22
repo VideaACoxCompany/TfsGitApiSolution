e3f9f67a1a8a656916db255730414816420120e4
Chu, Johnny (CMG-Videa) <Johnny.Chu@Videa.tv>
2018-01-22 11:53:33 -0500

    addtional changes

--- TfsGitApiSolution/TfsGitApiProject/Program.cs
+++ TfsGitApiSolution/TfsGitApiProject/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Net;
 using System.Net.Mail;
-using System.Security.Cryptography.X509Certificates;
-using Newtonsoft.Json;
-using TfsGitApiProject.Entities;
+using System.Text;
+using System.Threading.Tasks;
 using TfsGitApiProject.Services;
 
 namespace TfsGitApiProject
@@ -16,13 +14,17 @@ namespace TfsGitApiProject
         static void Main(string[] args)
         {
 
-            GetResults();
+            var result = GetResults().Result;
+
+            var emailer = new SmtpService();
+            emailer.SendEmail(result);
+            Console.WriteLine("Success!");
             Console.Read();
         }
 
 
 
-        public static async void GetResults()
+        public static async Task<string> GetResults()
         {
             try
             {
@@ -54,19 +56,27 @@ namespace TfsGitApiProject
                 var gitStatService = new GitStatsService();
                 var branchesInterested = new[] {"dev", "release-93"};
 
+
+                var result = new StringBuilder();
+
+
                 foreach (var repo in repos)
                 {
                     var gitStat = await gitStatService.GetResult(repo.Key);
                     gitStat.Value.Where(i => branchesInterested.Contains(i.Name)).ToList().ForEach(i =>
-                        Console.WriteLine($"Repo: {repo.Value.PadRight(20)} Branch {i.Name.PadRight(20)} is {i.BehindCount.PadRight(5)} commit(s) behind master branch"));
+                        //Console.WriteLine($"Repo: {repo.Value.PadRight(20)} Branch {i.Name.PadRight(20)} is {i.BehindCount.PadRight(5)} commit(s) behind master branch"));
+                            result.AppendLine(
+                                $"Repo: {repo.Value.PadRight(50)} Branch {i.Name.PadRight(20)} is {i.BehindCount.PadRight(5)} commit(s) behind master branch"));
 
                 }
+                return result.ToString();
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
+            return string.Empty;
         }
 
         
---
+++ TfsGitApiSolution/TfsGitApiProject/Services/SmtpService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace TfsGitApiProject.Services
+{
+    public class SmtpService
+    {
+        public void SendEmail(string body)
+        {
+
+            var fromAddress = new MailAddress("johnnyuhc@gmail.com", "Johnny Chu");
+            var toAddress = new MailAddress("johnny.chu@videa.tv", "Johnny Chu");
+
+            const string fromPassword = "abcd123";
+            const string subject = "Branch Verification";
+
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+                {
+
+                    Subject = subject,
+                    Body = body
+                }
+
+            )
+            {
+                smtp.Send(message);
+            }
+
+        }
+    }
+}
